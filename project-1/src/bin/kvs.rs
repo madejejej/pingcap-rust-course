ec76caa96c87e57c99a8cb77bbc1651622cfef91
Kacper Madej <kacperoza@gmail.com>
2020-03-22 16:47:45 +0100

    Use Structopt

--- project-1/src/bin/kvs.rs
+++ project-1/src/bin/kvs.rs
@@ -1,42 +1,20 @@
-#[macro_use]
-extern crate clap;
+use structopt::StructOpt;
 
-use kvs;
+#[derive(Debug, StructOpt)]
+#[structopt(name = "kvstore", about = "Simple key-value store")]
+enum Opt {
+    Get { key: String },
+    Set { key: String, value: String },
+    Rm { key: String },
+}
 
 fn main() {
-    let matches = clap_app!(kvstore =>
-      (version: env!("CARGO_PKG_VERSION"))
-      (author: env!("CARGO_PKG_AUTHORS"))
-      (@subcommand get =>
-       (@arg KEY: +required "Specifies which key should be fetched")
-       )
-      (@subcommand set =>
-       (@arg KEY: +required "Specifies which key should be changed")
-       (@arg VALUE: +required "Value to be saved")
-       )
-      (@subcommand rm =>
-       (@arg KEY: +required "Specifies which key should be removed")
-       )
-    )
-    .get_matches();
+    let opt = Opt::from_args();
 
-    let mut store = kvs::KvStore::new();
-
-    if let Some(matches) = matches.subcommand_matches("get") {
-        let key: String = matches.value_of("KEY").unwrap().to_string();
-        panic!("unimplemented");
-    } else if let Some(matches) = matches.subcommand_matches("set") {
-        let key = matches.value_of("KEY").unwrap().to_string();
-        let value = matches.value_of("VALUE").unwrap().to_string();
-
-        store.set(key, value);
-        panic!("unimplemented");
-    } else if let Some(matches) = matches.subcommand_matches("rm") {
-        let key = matches.value_of("KEY").unwrap().to_string();
-
-        store.remove(key);
-        panic!("unimplemented");
-    } else {
-        panic!();
+    match opt {
+        Opt::Get { key } => panic!("unimplemented"),
+        Opt::Set { key, value } => panic!("unimplemented"),
+        Opt::Rm { key } => panic!("unimplemented"),
+        _ => panic!(),
     }
 }
