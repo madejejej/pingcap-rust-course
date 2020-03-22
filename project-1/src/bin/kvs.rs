4bb17ee7afc04bea8c2a41ef5c4ff7d942365855
Kacper Madej <kacperoza@gmail.com>
2020-03-22 16:32:40 +0100

    Make all the tests pass

--- project-1/src/bin/kvs.rs
+++ project-1/src/bin/kvs.rs
@@ -1,3 +1,42 @@
+#[macro_use]
+extern crate clap;
+
+use kvs;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap_app!(kvstore =>
+      (version: env!("CARGO_PKG_VERSION"))
+      (author: env!("CARGO_PKG_AUTHORS"))
+      (@subcommand get =>
+       (@arg KEY: +required "Specifies which key should be fetched")
+       )
+      (@subcommand set =>
+       (@arg KEY: +required "Specifies which key should be changed")
+       (@arg VALUE: +required "Value to be saved")
+       )
+      (@subcommand rm =>
+       (@arg KEY: +required "Specifies which key should be removed")
+       )
+    )
+    .get_matches();
+
+    let mut store = kvs::KvStore::new();
+
+    if let Some(matches) = matches.subcommand_matches("get") {
+        let key: String = matches.value_of("KEY").unwrap().to_string();
+        panic!("unimplemented");
+    } else if let Some(matches) = matches.subcommand_matches("set") {
+        let key = matches.value_of("KEY").unwrap().to_string();
+        let value = matches.value_of("VALUE").unwrap().to_string();
+
+        store.set(key, value);
+        panic!("unimplemented");
+    } else if let Some(matches) = matches.subcommand_matches("rm") {
+        let key = matches.value_of("KEY").unwrap().to_string();
+
+        store.remove(key);
+        panic!("unimplemented");
+    } else {
+        panic!();
+    }
 }
--- project-1/src/lib.rs
+++ project-1/src/lib.rs
@@ -1,1 +1,28 @@
+use std::collections::HashMap;
 
+pub struct KvStore {
+    map: HashMap<String, String>,
+}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        let map: HashMap<String, String> = HashMap::new();
+
+        KvStore { map }
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        match self.map.get(&key) {
+            Some(value) => Some(value.clone()),
+            None => None,
+        }
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        self.map.insert(key, value);
+    }
+
+    pub fn remove(&mut self, key: String) {
+        self.map.remove(&key);
+    }
+}
