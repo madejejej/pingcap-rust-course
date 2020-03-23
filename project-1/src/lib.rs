ebb3500d10a02f8d95f637a905c5c003bc0a5ea5
Kacper Madej <kacperoza@gmail.com>
2020-03-23 19:24:20 +0100

    Make the tests compile

--- project-1/src/lib.rs
+++ project-1/src/lib.rs
@@ -1,8 +1,12 @@
 use std::collections::HashMap;
+use std::path::Path;
+use std::error;
 
 pub struct KvStore {
     map: HashMap<String, String>,
 }
+
+pub type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
 impl KvStore {
     pub fn new() -> KvStore {
@@ -11,18 +15,25 @@ impl KvStore {
         KvStore { map }
     }
 
-    pub fn get(&self, key: String) -> Option<String> {
+    pub fn get(&self, key: String) -> Result<Option<String>> {
         match self.map.get(&key) {
-            Some(value) => Some(value.clone()),
-            None => None,
+            Some(value) => Ok(Some(value.clone())),
+            None => Ok(None),
         }
     }
 
-    pub fn set(&mut self, key: String, value: String) {
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
         self.map.insert(key, value);
+        Ok(())
     }
 
-    pub fn remove(&mut self, key: String) {
+    pub fn remove(&mut self, key: String) -> Result<()> {
         self.map.remove(&key);
+        Ok(())
+    }
+
+    pub fn open(path: &Path) -> Result<KvStore> {
+        let map: HashMap<String, String> = HashMap::new();
+        Ok(KvStore { map })
     }
 }
