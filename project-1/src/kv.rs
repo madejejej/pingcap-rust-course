de8e6179cdd8f5524a5501cea9231f4140670ede
Kacper Madej <kacperoza@gmail.com>
2020-04-08 20:03:35 +0200

    Support multiple log files

--- project-1/src/kv.rs
+++ project-1/src/kv.rs
@@ -4,13 +4,16 @@ use serde_json;
 use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
 use std::io::{BufReader, BufWriter, Read, Seek, SeekFrom, Write};
-use std::path::Path;
+use std::path::{Path, PathBuf};
+
+const MAX_SEGMENT_SIZE: u64 = 1024 * 1024; // 1 MB
 
 pub struct KvStore {
     map: HashMap<String, CommandPosition>,
     writer: BufWriter<File>,
     current_generation: usize,
     readers: Vec<BufReader<File>>,
+    path: PathBuf,
 }
 
 #[derive(Debug)]
@@ -29,7 +32,11 @@ enum Operation {
 impl KvStore {
     pub fn get(&mut self, key: String) -> Result<Option<String>> {
         match self.map.get(&key) {
-            Some(CommandPosition { offset, length, generation }) => {
+            Some(CommandPosition {
+                offset,
+                length,
+                generation,
+            }) => {
                 let reader = &mut self.readers[*generation - 1];
 
                 reader.seek(SeekFrom::Start(*offset))?;
@@ -54,13 +61,29 @@ impl KvStore {
         let bytes = serde_json::to_vec(&op)?;
 
         let length = bytes.len() as u64;
-        let offset = self.writer.seek(SeekFrom::Current(0))?;
+        let mut offset = self.writer.seek(SeekFrom::Current(0))?;
+
+        if length + offset > MAX_SEGMENT_SIZE {
+            self.current_generation += 1;
+            let (reader, writer) = create_new_segment(&self.path, self.current_generation)?;
+            self.readers.push(reader);
+            self.writer = writer;
+            offset = self.writer.seek(SeekFrom::Current(0))?;
+        }
+
         let generation = self.current_generation;
 
         // TODO: handle partial writes and write errors
         self.writer.write(&bytes)?;
         self.writer.flush()?;
-        self.map.insert(key, CommandPosition { offset, length, generation });
+        self.map.insert(
+            key,
+            CommandPosition {
+                offset,
+                length,
+                generation,
+            },
+        );
         Ok(())
     }
 
@@ -79,52 +102,80 @@ impl KvStore {
     }
 
     pub fn open(path: &Path) -> Result<KvStore> {
-        let path = path.join("1.log");
-        let write_file = OpenOptions::new()
-            .append(true)
-            .create(true)
-            .write(true)
-            .open(&path)?;
-
-        let read_file = File::open(path)?;
-
-        let mut writer = BufWriter::with_capacity(512 * 1024, write_file);
-        writer.seek(SeekFrom::End(0))?;
-        let mut reader = BufReader::with_capacity(512 * 1024, read_file);
+        let mut current_generation = 1;
         let mut map: HashMap<String, CommandPosition> = HashMap::new();
-        let mut pos: u64 = reader.seek(SeekFrom::Start(0))?;
-        let mut stream =
-            serde_json::Deserializer::from_reader(&mut reader).into_iter::<Operation>();
+        let mut readers: Vec<BufReader<File>> = vec![];
 
-        let current_generation = 1;
+        loop {
+            let file_path = path.join(format!("{}.log", current_generation));
 
-        while let Some(operation) = stream.next() {
-            let new_pos = stream.byte_offset() as u64;
+            if !file_path.exists() {
+                break;
+            }
 
-            match operation? {
-                Operation::Set { key, value: _ } => map.insert(
-                    key,
-                    CommandPosition {
-                        offset: pos,
-                        length: new_pos - pos,
-                        generation: current_generation,
-                    },
-                ),
-                Operation::Remove { key } => map.remove(&key),
-            };
+            let read_file = File::open(file_path)?;
 
-            pos = new_pos;
+            if read_file.metadata()?.len() == 0 {
+              break;
+            }
+
+            let mut reader = BufReader::with_capacity(512 * 1024, read_file);
+            let mut pos: u64 = reader.seek(SeekFrom::Start(0))?;
+            let mut stream =
+                serde_json::Deserializer::from_reader(&mut reader).into_iter::<Operation>();
+
+            while let Some(operation) = stream.next() {
+                let new_pos = stream.byte_offset() as u64;
+
+
+                match operation? {
+                    Operation::Set { key, value: _ } => map.insert(
+                        key,
+                        CommandPosition {
+                            offset: pos,
+                            length: new_pos - pos,
+                            generation: current_generation,
+                        },
+                    ),
+                    Operation::Remove { key } => map.remove(&key),
+                };
+
+                pos = new_pos;
+            }
+
+            readers.push(reader);
+            current_generation += 1;
         }
 
-        let readers = vec![reader];
+        let (reader, writer) = create_new_segment(&PathBuf::from(path), current_generation)?;
+        readers.push(reader);
 
         let store = KvStore {
             map,
             writer,
             current_generation,
             readers,
+            path: PathBuf::from(path),
         };
 
         Ok(store)
     }
+}
+
+
+fn create_new_segment(path: &PathBuf, generation: usize) -> Result<(BufReader<File>, BufWriter<File>)> {
+    let path = path.join(format!("{}.log", generation));
+    let write_file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .write(true)
+        .open(&path)?;
+
+    let read_file = File::open(path)?;
+
+    let mut writer = BufWriter::with_capacity(512 * 1024, write_file);
+    writer.seek(SeekFrom::End(0))?;
+    let reader = BufReader::with_capacity(512 * 1024, read_file);
+
+    Ok((reader, writer))
 }
