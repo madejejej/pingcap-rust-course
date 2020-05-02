938bd530c72a2c308e8c4746171146a6ee890121
Kacper Madej <kacperoza@gmail.com>
2020-05-02 19:22:20 +0200

    Naive compaction template

--- project-1/src/kv.rs
+++ project-1/src/kv.rs
@@ -7,6 +7,7 @@ use std::io::{BufReader, BufWriter, Read, Seek, SeekFrom, Write};
 use std::path::{Path, PathBuf};
 
 const MAX_SEGMENT_SIZE: u64 = 1024 * 1024; // 1 MB
+const COMPACTION_THRESHOLD: u64 = 1024 * 1024; // 1MB
 
 pub struct KvStore {
     map: HashMap<String, CommandPosition>,
@@ -14,6 +15,7 @@ pub struct KvStore {
     current_generation: usize,
     readers: Vec<BufReader<File>>,
     path: PathBuf,
+    garbage_amount: u64,
 }
 
 #[derive(Debug)]
@@ -76,6 +78,12 @@ impl KvStore {
         // TODO: handle partial writes and write errors
         self.writer.write(&bytes)?;
         self.writer.flush()?;
+
+        if let Some(value) = self.map.get(&key) {
+            self.garbage_amount += value.length;
+            self.compact_me_maybe()?;
+        }
+
         self.map.insert(
             key,
             CommandPosition {
@@ -94,7 +102,11 @@ impl KvStore {
             let bytes = serde_json::to_vec(&op)?;
             self.writer.write(&bytes)?;
             self.writer.flush()?;
-            self.map.remove(&key).ok_or(KvError::KeyNotFound { key })?;
+            // assume we do single-threaded operations for now and then
+            // map will still contain the value
+            let val = self.map.remove(&key).unwrap();
+            self.garbage_amount += val.length;
+            self.compact_me_maybe()?;
             Ok(())
         } else {
             Err(KvError::KeyNotFound { key: key })
@@ -105,6 +117,7 @@ impl KvStore {
         let mut current_generation = 1;
         let mut map: HashMap<String, CommandPosition> = HashMap::new();
         let mut readers: Vec<BufReader<File>> = vec![];
+        let mut garbage_amount = 0;
 
         loop {
             let file_path = path.join(format!("{}.log", current_generation));
@@ -116,7 +129,7 @@ impl KvStore {
             let read_file = File::open(file_path)?;
 
             if read_file.metadata()?.len() == 0 {
-              break;
+                break;
             }
 
             let mut reader = BufReader::with_capacity(512 * 1024, read_file);
@@ -127,17 +140,28 @@ impl KvStore {
             while let Some(operation) = stream.next() {
                 let new_pos = stream.byte_offset() as u64;
 
-
                 match operation? {
-                    Operation::Set { key, value: _ } => map.insert(
-                        key,
-                        CommandPosition {
-                            offset: pos,
-                            length: new_pos - pos,
-                            generation: current_generation,
-                        },
-                    ),
-                    Operation::Remove { key } => map.remove(&key),
+                    Operation::Set { key, value: _ } => {
+                        if let Some(value) = map.get(&key) {
+                            garbage_amount += value.length;
+                        }
+
+                        map.insert(
+                            key,
+                            CommandPosition {
+                                offset: pos,
+                                length: new_pos - pos,
+                                generation: current_generation,
+                            },
+                        );
+                    }
+                    Operation::Remove { key } => {
+                        if let Some(value) = map.get(&key) {
+                            garbage_amount += value.length;
+                        }
+
+                        map.remove(&key);
+                    }
                 };
 
                 pos = new_pos;
@@ -156,14 +180,29 @@ impl KvStore {
             current_generation,
             readers,
             path: PathBuf::from(path),
+            garbage_amount,
         };
 
         Ok(store)
     }
+
+    fn compact_me_maybe(&self) -> Result<()> {
+        if self.garbage_amount < COMPACTION_THRESHOLD {
+            return Ok(());
+        }
+
+        // read logs in the loop, except the last one
+        // 1. deduplicate data in a single log
+        // 2. remove data if generation in hashmap is greater than the file
+
+        Ok(())
+    }
 }
 
-
-fn create_new_segment(path: &PathBuf, generation: usize) -> Result<(BufReader<File>, BufWriter<File>)> {
+fn create_new_segment(
+    path: &PathBuf,
+    generation: usize,
+) -> Result<(BufReader<File>, BufWriter<File>)> {
     let path = path.join(format!("{}.log", generation));
     let write_file = OpenOptions::new()
         .append(true)
